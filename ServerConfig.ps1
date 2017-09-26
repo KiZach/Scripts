1c0a2a26fdcfb2e6da62dc630fc40a5be002f8f2
Kim Zachariassen <kza@z4it.com>
2017-09-26 14:35:45 +0200

    Add files via upload

--- ServerConfig.ps1
+++ ServerConfig.ps1
@@ -1,5 +1,6 @@
 param([bool]$Datadisk)
 
 if ($Datadisk) {
+    write-host "Partition disk"
     Get-Disk | Where partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -DriveLetter F -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "ADDSDATA" -Confirm:$false
 }
