1fd4cf705ac88284916da677951ceac62b12092d
Kim Zachariassen <kza@z4it.com>
2017-09-27 08:40:05 +0200

    Add files via upload

--- ServerConfig.ps1
+++ ServerConfig.ps1
@@ -4,10 +4,21 @@
 #>
 
 Param(
-    [string]$Datadisk
+    [string]$Datadisk,
+    [string]$FirstDC,
+    [string]$FQDNDomainName,
+    [string]$NetbiosDomainName,
+    [string]$RestorePassword
 )
 
 if ($Datadisk -eq "true") {
     write-host "Partition disk"
     Get-Disk | Where partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -DriveLetter F -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "DATA" -Confirm:$false
+}
+
+if ($FirstDC -eq "true") {
+    $SafeModeAdministratorPassword = $RestorePassword | ConvertTo-SecureString -asPlainText -Force
+    write-host "Configuring first domain controller"
+    Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+    Install-ADDSForest -DomainName $FQDNDomainName -DomainNetbiosName $NetbiosDomainName -InstallDns --SafeModeAdministratorPassword $SafeModeAdministratorPassword -DatabasePath "F:\NTDS" -LogPath "F:\NTDS" -SysvolPath "F:\Sysvol"      
 }
